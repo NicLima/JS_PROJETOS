e5636e34e002da9f36d102482e0418fba2fb80ef
NicLima <nicolascarlos2304@gmail.com>
2024-05-24 08:29:23 -0300

    exercícios adicionados

---
+++ Aula13/loop.js
@@ -0,0 +1,19 @@
+var c = 0
+/*
+do{
+    console.log(`Passo = ${c}`)
+    c++
+} while(c <= 6)
+*/
+/*
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+console.log(`Vai comecar...`)
+for (var c = 1; c<= 4; c++){
+    console.log(c)
+}
+console.log(`Fim`)
