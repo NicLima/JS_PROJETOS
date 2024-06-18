9243bc5beb21b388bf51cb8dcf9fc131bdbca6e5
Nicolas Lima <nicolascarlos2304@gmail.com>
2024-06-18 08:21:11 -0300

    retomar estudos

---
+++ Aula11/EX008.js
@@ -0,0 +1,9 @@
+var vel = 62
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if ( vel > 60){
+    console.log(`A velocidade permitida é de 60 Km/h`)
+    console.log(`Você ultrapassou a velocidade permitida, multado!`)
+}
+
